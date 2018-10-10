89ba34a6a589db529209b4978a3b7ff7116b8a6f
Alexandre Martinez <alexandre.martinez@benextcompany.com>
2018-10-10 11:17:18 +0200

    Display list of comments with dynamic values

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,11 +2,19 @@ import React, {Component} from "react";
 
 export class CommentList extends Component {
     render() {
+
+        const comments = [{
+            id: 'ABCD',
+            message: 'Comment A'
+        },{
+            id: '99F3',
+            message:'Comment B'
+        }];
+
         return <div className="comment-list">
             <h1>Commentaires</h1>
             <ul>
-                <li>COMMENT</li>
-                <li>COMMENT</li>
+                { comments.map(({id, message}) => <li key={id}>{message}</li>)}
             </ul>
         </div>;
     }
