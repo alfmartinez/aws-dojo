b15edadcd26ef5e57aeb7621fc87417c2aeb37d6
Alexandre Martinez <alexandre.martinez@benextcompany.com>
2018-10-10 11:01:47 +0200

    Update form state

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,15 +1,28 @@
 import React, {Component} from "react";
 
 export class CommentForm extends Component {
+
+
+    constructor() {
+        super();
+        this.state = {
+            message: ''
+        };
+    }
+
+    updateForm = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value});
+    }
+
     render() {
         return <form>
             <fieldset>
-                <field>
-                    <label>Message : </label>
-                    <input type="text" name="message"/>
-                </field>
-                <input type="submit" value="Envoyer"/>
+                <label>Message : </label>
+                <input type="text" name="message" value={this.state.message} onChange={this.updateForm}/>
             </fieldset>
+            <input type="submit" value="Envoyer"/>
         </form>;
     }
 }
